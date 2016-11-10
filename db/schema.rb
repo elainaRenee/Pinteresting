352d6c4cfdba1cff8c7d12377804a64c4cafe755
Elaina Hodgkin <ehodgkin9418@gmail.com>
2016-11-10 12:58:17 -0600

    created pins model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161108225124) do
+ActiveRecord::Schema.define(version: 20161110184300) do
+
+  create_table "pins", force: :cascade do |t|
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
