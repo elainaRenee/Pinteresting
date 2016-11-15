f84acc3f0b162ce703fbf432a683245bd1aa5cfd
Elaina Hodgkin <ehodgkin9418@gmail.com>
2016-11-14 20:46:30 -0600

    added amazon web services

--- Gemfile
+++ Gemfile
@@ -23,6 +23,8 @@ gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
 
 gem 'paperclip'
+
+gem 'aws-sdk', '< 2.0'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 3.0'
 # Use ActiveModel has_secure_password
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,4 +86,14 @@ Rails.application.configure do
 
   #required for heroku
   config.action_mailer.default_url_options = { :host => 'my-pinteresting-app.herokuapp.com' }
+
+  #sets paperclip to upload images to Amazon S3
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['S3_BUCKET_NAME'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
 end
