58a6b69448b07a7e0c0d4a00f1b08cb91251fd2c
Elaina Hodgkin <ehodgkin9418@gmail.com>
2016-11-14 21:55:31 -0600

    added pins to home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :pins
   devise_for :users
-  root "pages#home"
+  root "pins#index"
   get "about" => "pages#about"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
