d2dd2d3e346b7d0f6d4bfad71cb97734bdb8b1a5
Elaina Hodgkin <ehodgkin9418@gmail.com>
2016-11-14 22:40:16 -0600

    add names to users

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,4 +2,6 @@ class Pin < ActiveRecord::Base
 	belongs_to :user
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+	validates :image, presence: true
+  validates :description, presence: true
 end
